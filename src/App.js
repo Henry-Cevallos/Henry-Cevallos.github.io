e5f4911d4588403d76002d7bd0b0936318bcbf46
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-01-04 09:30:24 -0500

    added avatar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Avatar } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>Hello</div>
+        <Avatar className='Avatar' alt='henry cevallos'/>
       </header>
     </div>
   );

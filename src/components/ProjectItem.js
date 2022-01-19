0c1a69b113f772780d0ccb882b4015d878e6e475
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-01-18 21:32:36 -0500

    added component file

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import '../styles/ProjectsItem.css';
+
+const ProjectItem = ( {title}) => {
+
+    return(
+        <div className="project-item">
+            {title}
+        </div>
+    )
+    
+}
+
+export default ProjectItem;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,14 @@
 import React from "react";
 import '../styles/Projects.css';
+import ProjectItem from "./ProjectItem";
+
 const Projects = () => {
 
     return (
         <div className='Projects-start'>
-            Hello
+            <div className='project-card'>
+                <ProjectItem />
+            </div>
         </div>
     )
 }

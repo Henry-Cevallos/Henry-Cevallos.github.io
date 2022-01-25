678758fdd36f191bd3855e15bb4cb64c26dc9133
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2022-01-24 21:56:12 -0500

    added come scaling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,9 +5,9 @@ import ProjectItem from "./ProjectItem";
 const Projects = () => {
 
     return (
-        <div className='Projects-start'>
+        <div className='projects-start'>
             <div className='project-card'>
-                <ProjectItem title='Schedule.Friends'/>
+                <ProjectItem  className='this-one' title='Schedule.Friends'/>
                 <ProjectItem title='Travelers Log'/>
                 <ProjectItem title='Web Dev'/>
                 <ProjectItem title='Spell Checker'/>
